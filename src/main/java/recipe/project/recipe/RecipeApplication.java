2c4f64394273b6a7e3361e95991034c1b712d538
darkonion <pawkapl89@gmail.com>
2020-04-26 22:31:32 +0200

    added some tests

--- src/main/java/recipe/project/recipe/RecipeApplication.java
+++ src/main/java/recipe/project/recipe/RecipeApplication.java
@@ -2,11 +2,12 @@ package recipe.project.recipe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RecipeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RecipeApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(RecipeApplication.class, args);
     }
 }
---
+++ src/test/java/recipe/project/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,48 @@
+package recipe.project.recipe.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import recipe.project.recipe.domain.Recipe;
+import recipe.project.recipe.repositories.RecipeRepository;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet recipesData = new HashSet();
+        recipesData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+
+    }
+}
